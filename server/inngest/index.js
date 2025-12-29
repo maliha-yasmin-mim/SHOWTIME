3d2be2b1eea31a95af325d4de7e285004c6985de
maliha-yasmin-mim <malihayasmin01.official@gmail.com>
2025-12-29 17:35:33 +0600

    booking confermation add

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -2,6 +2,7 @@ import { Inngest } from "inngest";
 import User from "../models/User.js";
 import Booking from "../models/Booking.js";
 import Show from "../models/Show.js";
+import sendEmail from "../configs/nodeMailer.js";
 
 
 // Create a client to send and receive events
@@ -81,7 +82,41 @@ const releaseSeatsAndDeleteBooking = inngest.createFunction(
 );
 
 
+// Inngest Function to send email when user books a show
+const sendBookingConfirmationEmail = inngest.createFunction(
+  { id: "send-booking-confirmation-email" },
+  { event: "app/show.booked" },
+  async ({ event, step }) => {
+    const { bookingId } = event.data;
+    const booking = await Booking.findById(bookingId)
+      .populate({
+        path: "show",
+        populate: { path: "movie", model: "Movie" },
+      })
+      .populate("user");
 
+    await sendEmail({
+      to: booking.user.email,
+      subject: `Payment Confirmation: "${booking.show.movie.title}" booked!`,
+      body: `<div style="font-family: Arial, sans-serif; line-height: 1.5;">
+        <h2>Hi ${booking.user.name},</h2>
+        <p>Your booking for <strong style="color: #F84565;">"${
+          booking.show.movie.title
+        }"</strong> is confirmed.</p>
+        <p>
+          <strong>Date:</strong> ${new Date(
+            booking.show.showDateTime
+          ).toLocaleDateString("en-US", { timeZone: "Africa/Kigali" })}<br />
+          <strong>Time:</strong> ${new Date(
+            booking.show.showDateTime
+          ).toLocaleTimeString("en-US", { timeZone: "Africa/Kigali" })}
+        </p>
+        <p>Enjoy the show! 🍿</p>
+        <p>Thanks for booking with us!<br />- QuickShow Team</P>
+      </div>`,
+    });
+  }
+);
 
 
 
@@ -97,7 +132,7 @@ const releaseSeatsAndDeleteBooking = inngest.createFunction(
 
 
 export const functions = [syncUserCreation , syncUserDeletion , 
-syncUserUpdation, releaseSeatsAndDeleteBooking
+syncUserUpdation, releaseSeatsAndDeleteBooking, sendBookingConfirmationEmail
 
 
 ];
